4e9107aacc9c5b0304e1be363edc1dd8854a4dce
Javi Aranda <j.arandaizquierdo@gmail.com>
2019-02-18 01:31:29 +0100

    Creating and getting ready parctice 1

---
+++ Practices/Practice1/main.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.cpp
+ * Author: javierarandaizquierdo
+ *
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ Practices/Practice1/vehicle.cpp
@@ -0,0 +1,23 @@
+#include <vehicle.h>
+using namespace Vehicle;
+        
+int fillVector( Vehicle v[], int lenV )
+{
+    
+};
+
+showOnScreen(Vehicle v[], int lenV)
+{
+
+};
+
+maxPrice(Vehicle v[], int lenV)
+{
+    
+
+};
+
+searchByPlate(string plateNumber, Vehicle v[], int lenV)
+{
+
+};
